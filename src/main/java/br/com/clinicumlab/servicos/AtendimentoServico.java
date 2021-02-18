d8f80bf63c3487c4d0c239165b1448932de45bc4
cassioliveira <cassio@cassioliveira.com.br>
2021-02-17 21:38:33 -0300

    Gerado protocolo de atendimento

--- src/main/java/br/com/clinicumlab/servicos/AtendimentoServico.java
+++ src/main/java/br/com/clinicumlab/servicos/AtendimentoServico.java
@@ -5,10 +5,15 @@ import br.com.clinicumlab.enumeracao.Estados;
 import br.com.clinicumlab.enumeracao.StatusAtendimento;
 import br.com.clinicumlab.modelo.Atendimento;
 import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
+import java.util.Random;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import lombok.Getter;
@@ -34,8 +39,9 @@ public class AtendimentoServico implements Serializable {
 
     @Transactional
     public void salvar(Atendimento atendimento) {
-        if (atendimento.getId() == null) {
+        if (atendimento.getId() == null) { 
             atendimento.setDataAtendimento(new Date());
+            atendimento.setProtocolo(geraProtocolo());
         }
         this.atendimentoDao.save(atendimento);
     }
@@ -61,6 +67,20 @@ public class AtendimentoServico implements Serializable {
             }
         }
         return atendimentosAbertos;
+    }
+
+    /**
+     * Gera um número de protocolo para o atendimento. Esse número é composto pela
+     * data de cadastro do atendimento seguido de um número aleatório de 5 dígitos.
+     * 
+     * @return 
+     */
+    private String geraProtocolo() {
+        String protocolo;
+        Random aleatorio = new Random();
+        String mesAno = LocalDate.now().format(DateTimeFormatter.BASIC_ISO_DATE); //Gera a data de hoje formatada somente com números
+        protocolo = mesAno + (aleatorio.nextInt(99000) + 10000); //Última parte gera um número a partir de 10000 e menor que 99999
+        return protocolo;
     }
 
 }
